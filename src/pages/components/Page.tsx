28097feae99df55cd8c3b3e6dd29c376961a9513
Dylan Masters <dmasters@csumb.edu>
2023-10-13 13:19:29 -0700

    updated button syntax for nav and pageselect

--- src/pages/components/Page.tsx
+++ src/pages/components/Page.tsx
@@ -1,70 +1,32 @@
 import Link from "next/link";
+import Button from "./button";
 
 export const Precruise = () => {
   return (
-    <Link
-      href="PreCruiseForm"
-      className="inline-block group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
-    >
-      <div className="flex items-center justify-center h-full">
-        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-          Pre-Cruise
-        </span>
-      </div>
-    </Link>
+    <Button hrefLink="/PreCruiseForm" buttonName="Pre Cruise"/>
   );
 };
+
 export const Postcruise = () => {
   return (
-    <Link
-      href="PostCruiseForm"
-      className="inline-block group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
-    >
-      <div className="flex items-center justify-center h-full">
-        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-          Post-Cruise
-        </span>
-      </div>
-    </Link>
+    <Button hrefLink="/PostCruiseForm" buttonName="Post Cruise"/>
   );
 };
+
 export const Reports = () => {
   return (
-    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
-      <div className="relative flex items-center space-x-4 justify-center">
-        <a href="">
-          <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-            Reports
-          </span>
-        </a>
-      </div>
-    </button>
+    <Button hrefLink="" buttonName="Reports Search"/>
   );
 };
+
 export const Database = () => {
   return (
-    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
-      <div className="relative flex items-center space-x-4 justify-center">
-        <a href="">
-          <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-            Database
-          </span>
-        </a>
-      </div>
-    </button>
+    <Button hrefLink="" buttonName="Database"/>
   );
 };
 
 export const Logout = () => {
   return (
-    <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
-      <div className="relative flex items-center space-x-4 justify-center">
-        <a href="">
-          <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-            Logout
-          </span>
-        </a>
-      </div>
-    </button>
+    <Button hrefLink="" buttonName="Logout"/>
   );
 };
