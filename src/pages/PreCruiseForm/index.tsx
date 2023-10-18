71e2d2999f738886305479f38a0191808a7f6246
More3428 <andmoreno@csumb.edu>
2023-10-17 21:22:35 -0700

    Finished PreCruiseForm

--- src/pages/PreCruiseForm/index.tsx
+++ src/pages/PreCruiseForm/index.tsx
@@ -6,8 +6,192 @@ import Navbar from "../components/Navbar";
 
 const preCruiseForm = () => {
   return (
-    <div>
-      <Navbar currentPage="precruise" />
+    <div className="h-screen overflow-y-auto">
+      <Navbar currentPage="precruise" className="sticky top-0 z-10" />
+      <div className="bg-gray-100 flex flex-col items-center justify-center font-sans text-cyan-900 pt-24">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-lg">
+          <h1 className="mb-2 text-2xl text-center text-cyan-900 font-bold">Pre-Cruise Form</h1>
+          <form method="post">
+            <fieldset>
+              <p className="mb-4 text-sm text-center italic">
+                Please fill out all the information below in order to submit a
+                request for a pre cruise approval.
+              </p>
+
+              <label
+                htmlFor="shipName"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Ship Name:
+                <select
+                  id="shipName"
+                  className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  required
+                >
+                  <option value="">(Select One)</option>
+                  <option value="1">Ship 1</option>
+                  <option value="2">Ship 2</option>
+                  <option value="3">Ship 3</option>
+                </select>
+              </label>
+              <br />
+              <label
+                htmlFor="purpose"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Purpose for cruise:
+                <br />
+                <textarea
+                  id="purpose"
+                  name="purpose"
+                  rows={3}
+                  cols={30}
+                  className="w-full border max-h-[100px] rounded-md p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Purpose of cruise (up to 8000 character text field)"
+                  required
+                ></textarea>
+              </label>
+
+              <label
+                htmlFor="chiefScientist"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Chief Scientist:
+                <select
+                  id="chiefScientist"
+                  className="block appearance-none w-full bg-white border border-gray-400  hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  required
+                >
+                  <option value="">(Select One)</option>
+                  <option value="1">Scientist 1</option>
+                  <option value="2">Scientist 2</option>
+                  <option value="3">Scientist 3</option>
+                </select>
+              </label>
+              <br />
+              <label
+                htmlFor="principalInvestigator"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Principal Investigator:
+                <select
+                  id="principalInvestigator"
+                  className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  required
+                >
+                  <option value="">(Select One)</option>
+                  <option value="1">Investigator 1</option>
+                  <option value="2">Investigator 2</option>
+                  <option value="3">Investigator 3</option>
+                </select>
+              </label>
+              <br />
+              <label
+                htmlFor="scheduledStartDatetime"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Scheduled Start Date time:
+                <div className="relative max-w-sm">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </div>
+                  <input
+                    type="date"
+                    id="scheduledEndDatetime"
+                    name="scheduledEndDatetime"
+                    className=" shadow leading-tight focus:outline-none focus:shadow-outline border border-gray-300  text-cyan-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+                    required
+                  ></input>
+                </div>
+              </label>
+              <br />
+              <label
+                htmlFor="scheduledEndDatetime"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Scheduled End Date time:
+                <div className="relative max-w-sm">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </div>
+                  <input
+                    type="date"
+                    id="scheduledEndDatetime"
+                    name="scheduledEndDatetime"
+                    className=" shadow leading-tight focus:outline-none focus:shadow-outline border border-gray-300  text-cyan-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  ></input>
+                </div>
+              </label>
+              <br />
+              <label
+                htmlFor="equipmentDescription"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Equipment Description:
+                <textarea
+                  id="equipmentDescription"
+                  name="equipmentDescription"
+                  rows={3}
+                  cols={30}
+                  className="w-full border max-h-[100px] rounded-md p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Equipment Description (up to 8000 character text field)"
+                  required
+                ></textarea>
+              </label>
+              <br />
+              <label
+                htmlFor="participants"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Participants:
+                <textarea
+                  id="participants"
+                  name="participants"
+                  rows={3}
+                  cols={30}
+                  className="w-full border max-h-[100px] rounded-md p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="participants (up to 8000 character text field)"
+                  required
+                ></textarea>
+              </label>
+              <br />
+              <label
+                htmlFor="regionDescription"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Region Description:
+                <textarea
+                  id="regionDescription"
+                  name="regionDescription"
+                  rows={3}
+                  cols={30}
+                  className="w-full border max-h-[150px] rounded-md p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Region Description (up to 2048 character text field)"
+                  required
+                ></textarea>
+              </label>
+              <br />
+              <label
+                htmlFor="plannedTrackDescription"
+                className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+              >
+                Planned Track Description:
+                <textarea
+                  id="plannedTrackDescription"
+                  name="plannedTrackDescription"
+                  rows={3}
+                  cols={30}
+                  className="w-full border max-h-[150px] rounded-md p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Planned Track Description (up to 6144 character text field)"
+                  required
+                ></textarea>
+              </label>
+            </fieldset>
+            <input type="submit" className="bg-cyan-900 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full shadow leading-tight focus:outline-none focus:shadow-outline" value="Submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -28,127 +28,144 @@ function classNames(...classes: string[]) {
 }
 type NavbarProps = {
   currentPage: "postcruise" | "precruise";
+  className?: string;
 };
 
-const Navbar: React.FC<NavbarProps> = ({ currentPage }) => {
+const Navbar: React.FC<NavbarProps> = ({ currentPage, className }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <header className="bg-white">
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              className="absolute top-0 left-0 h-40 w-auto"
-              src="/MBARI-Logo.png"
-              alt="mbarilogo"
-            />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a
-            href="/PageSelect"
-            className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
-          >
-            Home
-          </a>
-          <a
-            href={
-              currentPage === "postcruise"
-                ? "/PreCruiseForm"
-                : "/PostCruiseForm"
-            }
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            {currentPage === "postcruise" ? "Pre-Cruise" : "Post-Cruise"}
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Reports
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Dives
-          </a>
-        </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log Out <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </nav>
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
+    <>
+      <div className={`some-navbar-styles ${className}`}></div>
+      <header className="bg-white fixed top-0 left-0 w-full z-50">
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">MBARI</span>
-              <h2 className="mb-2 text-2xl text-center text-cyan-900 font-bold">
-                MBARI
-              </h2>
+              <span className="sr-only">Your Company</span>
+              <img
+                className="absolute top-0 left-0 h-40 w-auto md:block hidden"
+                src="/MBARI-Logo.png"
+                alt="mbarilogo"
+              />
             </a>
+          </div>
+          <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href={
-                    currentPage === "postcruise" ? "/precruise" : "/postcruise"
-                  }
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  {currentPage === "postcruise" ? "Pre-Cruise" : "Post-Cruise"}
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Reports
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Dives
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log Out
-                </a>
+          <Popover.Group className="hidden lg:flex lg:gap-x-12">
+            <a
+              href="/PageSelect"
+              className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
+            >
+              Home
+            </a>
+            <a
+              href={
+                currentPage === "postcruise"
+                  ? "/PreCruiseForm"
+                  : "/PostCruiseForm"
+              }
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {currentPage === "postcruise" ? "Pre-Cruise" : "Post-Cruise"}
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Reports
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Dives
+            </a>
+          </Popover.Group>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Log Out <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-10" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">MBARI</span>
+                <h2 className="mb-2 text-2xl text-center text-cyan-900 font-bold">
+                  MBARI
+                </h2>
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <a
+                    href={
+                      currentPage === "postcruise"
+                        ? "/precruise"
+                        : "/postcruise"
+                    }
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {currentPage === "postcruise"
+                      ? "Pre-Cruise"
+                      : "Post-Cruise"}
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Reports
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Dives
+                  </a>
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log Out
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+    </>
   );
 };
 
