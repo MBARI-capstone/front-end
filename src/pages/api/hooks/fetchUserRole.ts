f2b8c82ce6960f9f8d80e98f9a76750f101c0bb0
Yuki Okamoto <yokamoto@csumb.edu>
2023-11-14 20:58:47 -0800

    fix

--- src/pages/api/hooks/fetchUserRole.ts
+++ src/pages/api/hooks/fetchUserRole.ts
@@ -2,7 +2,7 @@ import { UserRoleType } from '../../components/store'
 
 const API_URL = 'http://localhost:8080/api/v1.1/auth/userRole'
 
-export async function fetchUserRole(context: any): Promise<any> {
+export async function fetchUserRole(context: any): Promise<UserRoleType> {
   try {
     const response = await fetch(API_URL, {
       method: 'GET',
