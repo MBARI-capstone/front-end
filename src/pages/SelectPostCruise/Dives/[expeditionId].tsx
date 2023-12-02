1addbf45aa0b0b74327fac9b67da3dc8f3e2a18f
Yuki Okamoto <yokamoto@csumb.edu>
2023-12-02 00:34:24 -0800

    feat: dive form

--- src/pages/SelectPostCruise/Dives/[expeditionId].tsx
+++ src/pages/SelectPostCruise/Dives/[expeditionId].tsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar'
 import BackButton from '../../components/BackButton'
+import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 
-const Dives = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const scientistsResponse = await fetch(
+    'http://localhost:8080/api/v1.1/data/allUsers',
+    {
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json',
+        Cookie: context.req.headers.cookie || '',
+      },
+    }
+  )
+
+  const scientists = await scientistsResponse.json()
+
+  return {
+    props: {
+      scientists,
+    },
+  }
+}
+interface Scientist {
+  userId: number
+  firstName: string
+  lastName: string
+}
+interface ScientistProps {
+  scientists: Scientist[]
+}
+const Dives: React.FC<ScientistProps> = ({ scientists }) => {
+  const router = useRouter()
+  const expeditionId = router.query.expeditionId
+  const [rovName, setRovName] = useState('')
+  const [diveNumber, setDiveNumber] = useState('')
+  const handleSubmit = async (event: any) => {
+    event.preventDefault()
+
+    const rovDiveDto = {
+      expeditionId: expeditionId,
+      rovName: rovName,
+      diveNumber: diveNumber,
+      diveStartDatetime: event.target.diveTimeStart.value,
+      diveEndDatetime: event.target.diveTimeEnd.value,
+      diveChiefScientistId: parseInt(event.target.chiefScientist.value, 10),
+      briefAccomplishments: event.target.acomplishments.value,
+    }
+
+    const response = await fetch('http://localhost:8080/api/v1.1/rovDive', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(rovDiveDto),
+    })
+
+    if (!response.ok) {
+      console.error('Failed to add dive')
+    } else {
+      console.log('Dive added successfully')
+    }
+  }
+
   return (
     <div className="h-screen  overflow-y-auto ">
       <Navbar currentPage="precruise" className="sticky top-0 z-10" />
@@ -16,18 +79,34 @@ const Dives = () => {
             Please fill out all the information below in order to add a dive to
             this cruise.
           </p>
-          <form method="post">
+          <form method="post" onSubmit={handleSubmit}>
             <label
               htmlFor="rovName"
               className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
             >
               ROV Name:
-              <select id="rovName" className="border rounded-md ml-2" required>
-                <option value="">(Select One)</option>
-                <option value="1">ROV 1</option>
-                <option value="2">ROV 2</option>
-                <option value="3">ROV 3</option>
-              </select>
+              <input
+                type="text"
+                id="rovName"
+                value={rovName}
+                onChange={(e: any) => setRovName(e.target.value)}
+                className="border rounded-md ml-2 p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                required
+              />
+            </label>
+            <label
+              htmlFor="diveNumber"
+              className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2"
+            >
+              Dive Number:
+              <input
+                type="text"
+                id="diveNumber"
+                value={diveNumber}
+                onChange={(e: any) => setDiveNumber(e.target.value)}
+                className="border rounded-md ml-2 p-2 shadow leading-tight focus:outline-none focus:shadow-outline"
+                required
+              />
             </label>
             <p className="block uppercase tracking-wide text-cyan-900 text-md font-bold mb-2">
               Time of Dive:
@@ -65,9 +144,12 @@ const Dives = () => {
                 required
               >
                 <option value="">(Select One)</option>
-                <option value="1">Scientist 1</option>
-                <option value="2">Scientist 2</option>
-                <option value="3">Scientist 3</option>
+                {scientists &&
+                  scientists.map((scientist) => (
+                    <option key={scientist.userId} value={scientist.userId}>
+                      {scientist.firstName} {scientist.lastName}
+                    </option>
+                  ))}
               </select>
             </label>
             ​
@@ -91,12 +173,12 @@ const Dives = () => {
         </label> */}
             <div className="flex flex-wrap">
               {/* This creates a flexible space */}
-              <BackButton hrefLink="/PageSelect" buttonName="Back" />
+              <BackButton hrefLink="/SelectPostCruise" buttonName="Back" />
               <div className="flex-1"></div>{' '}
               <input
                 type="submit"
                 className="bg-cyan-900 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full shadow leading-tight focus:outline-none focus:shadow-outline mx-2"
-                value="Add"
+                value="Add Dive"
               />
             </div>
           </form>
