c3d783dc2a543bfc0385388ed684c61c2031c8da
More3428 <andmoreno@csumb.edu>
2023-12-05 23:34:30 -0800

    SEarch UI

--- src/pages/components/BackButton.tsx
+++ src/pages/components/BackButton.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
     return (
       <a
         href={hrefLink}
-        className="bg-custom-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-cyan-900 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full shadow leading-tight focus:outline-none focus:shadow-outline"
       >
         {buttonName}
       </a>
